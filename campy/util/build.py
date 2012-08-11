5e6c2759c45f3c2861a9f786e13bae9ec6c78aaf
burak bayramli <burak@burak-Aspire-S3.(none)>
2012-08-11 19:32:23 +0200

    -

--- campy/util/build.py
+++
@@ -1,5 +0,0 @@
-import os, sys
-
-if sys.argv[1] == 'zip':
-    os.system("zip ~/Dropbox/Public/skfiles/siftpy.zip -r ./siftpp ./siftpy --exclude=*.so --exclude=*.o --exclude=*.avi --exclude=./help/* ")
-    
--- campy/util/snip.sh
+++
@@ -1,7 +0,0 @@
-# parameters: [hh:mm:ss] [ss]
-echo "---------------------------------------------"
-echo "File: $1"
-echo "Start: $2"
-echo "Duration: $3 seconds"
-echo "---------------------------------------------"
-mencoder -ss $2 -endpos $3 -oac copy -ovc copy $1 -o out.avi
