656cf821d2e636ea5c33959ba25fea6b91f61f46
burak bayramli <burak@burak-Aspire-S3.(none)>
2012-12-01 13:04:02 +0100

    -

--- delltest/run.sh
+++ delltest/run.sh
@@ -1,11 +1,12 @@
 #sh $HOME/bin/run_dell.sh part.sql > $HOME/dell.csv
 
-head -1 $HOME/dell.csv > $HOME/dell2.csv
-sed -n 2,30000p $HOME/dell.csv >> $HOME/dell2.csv
+#head -1 $HOME/dell.csv > $HOME/dell2.csv
+#sed -n 2,30000p $HOME/dell.csv >> $HOME/dell2.csv
 
-head -1 $HOME/dell.csv > $HOME/dell-validate.csv
-sed -n 30000,60000p $HOME/dell.csv >> $HOME/dell-validate.csv
+#head -1 $HOME/dell.csv > $HOME/dell-validate.csv
+#sed -n 30000,60000p $HOME/dell.csv >> $HOME/dell-validate.csv
 
 #R -f pred.R
 #R -f predglm.R
-R -f prednn.R
+#R -f prednn.R
+R -f predlogreg.py
