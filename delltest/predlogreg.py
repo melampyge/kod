543dca9e52d26639f81cff1229e8791c21834d62
burak bayramli <burak@burak-Aspire-S3.(none)>
2012-12-02 13:21:08 +0100

    -

--- delltest/predlogreg.py
+++ delltest/predlogreg.py
@@ -2,7 +2,7 @@ data <- read.csv ("/home/burak/dell2.csv",header=TRUE,sep=",")
 attach (data)
 
 dormant <- rep(0, length(data$last_visit))
-dormant[data$last_visit > 150 & (data$rank == data$per_customer_count | data$rank == data$per_customer_count-1)] = 1
+dormant[data$last_visit > 150 & data$rank == data$per_customer_count] = 1
 
 fit.1 <- glm(dormant ~ month + netamount + gender + 
       	    day + day_of_week + totalamount + rank + categoryname +
@@ -16,7 +16,7 @@ print (summary(fit.1))
 data.validate <- read.csv ("/home/burak/dell-validate.csv",header=TRUE,sep=",")
 attach(data.validate)
 dormant <- rep(0, length(data.validate$rank))
-dormant[data$last_visit > 150 & (data$rank == data$per_customer_count | data$rank == data$per_customer_count-1)] = 1
+dormant[data.validate$last_visit > 150 & data.validate$rank == data.validate$per_customer_count] = 1
 total <- length(data.validate$rank)
 
 g <- (predict(fit.1, type="response", data=data.validate)  >= 0.5)
