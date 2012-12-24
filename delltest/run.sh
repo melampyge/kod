2db8868811a26e386ae413623832a2823aea029a
burak bayramli <burak@burak-Aspire-S3.(none)>
2012-12-24 21:38:47 +0100

    -

--- delltest/run.sh
+++ delltest/run.sh
@@ -2,11 +2,12 @@
 #R -f conv.R
 
 head -1 $HOME/dell.csv > $HOME/dell-train.csv
-sed -n 2,30000p $HOME/dell.csv >> $HOME/dell-train.csv
+sed -n 2,50000p $HOME/dell.csv >> $HOME/dell-train.csv
 head -1 $HOME/dell.csv > $HOME/dell-validate.csv
-sed -n 30000,60000p $HOME/dell.csv >> $HOME/dell-validate.csv
+sed -n 50000,60000p $HOME/dell.csv >> $HOME/dell-validate.csv
 
 #R -f pred.R
 #R -f predglm.R
 #R -f prednn.R
-python predridge.py
+#python predridge.py
+python predknn.py
