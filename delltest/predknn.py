03aae12039f9e98368e8b5f2d42fb1881f2240e1
burak bayramli <burak@burak-Aspire-S3.(none)>
2012-12-27 11:33:49 +0100

    -

--- delltest/predknn.py
+++ delltest/predknn.py
@@ -7,13 +7,12 @@ from pandas import *
 __day__ = 130
 
 cols = """
-0 + month + day + day_of_week + rank + categoryname + time_on_site +
-per_customer_count + income + season + cat_freq + creditcardtype
+0 + month + day + day_of_week + rank + categoryname + income + season
++ cat_freq + creditcardtype
 """
 
 orders = read_csv("/home/burak/dell-train.csv",sep=",")
 train = dmatrix(cols,orders)
-print train.shape
 last_value = orders[['last_visit']].as_matrix()[:,0] > __day__
 
 print len(last_value[last_value==True])
@@ -24,15 +23,11 @@ knn.fit(train,last_value)
 
 orders = read_csv("/home/burak/dell-validate.csv",sep=",")
 validate = dmatrix(cols,orders)
-print validate.shape
 last_value = orders[['last_visit']].as_matrix()[:,0] > __day__
 
 pred = knn.predict(validate)
 
-success = 0
-for i,x in enumerate(pred):
-    if x==0 and last_value[i]==False: success += 1
-    if x==1 and last_value[i]==True: success += 1
-
+success = sum(pred[last_value==True] == 1) + \
+          sum(pred[last_value==False] == 0)
 print success / float(len(pred)) * 100
 
