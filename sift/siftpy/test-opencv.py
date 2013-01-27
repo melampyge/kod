d28b00e70437b56968c102bbc4d78b49168e3fad
burak bayramli <burak@burak-Aspire-S3.(none)>
2013-01-27 17:47:33 +0100

    -

--- sift/siftpy/test-opencv.py
+++ sift/siftpy/test-opencv.py
@@ -1,10 +1,8 @@
 import Image
 import sys
-from numpy import *
+import numpy as np
 from siftpy import *
-from opencv.cv import *
-from opencv.highgui import *
-from opencv.adaptors import *
+import cv
 import copy
 __start__ = 44
 __scale__ = 1
@@ -28,16 +26,16 @@ if __name__ == "__main__":
     print "saved in a snap.jpg file"
 
     # create windows
-    cvNamedWindow('Camera')
+    cv.NamedWindow('Camera')
  
     # create capture device
     device = 0 # assume we want first device
 
     #capture = cvCreateFileCapture ("~/Dropbox/Public/skfiles/campy/chessb-left.avi")
-    capture = cvCreateCameraCapture (0)
+    capture = cv.CreateCameraCapture (0)
     
-    cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH, 640)
-    cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, 480)    
+    cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH, 640)
+    cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_HEIGHT, 480)    
     
     # check if capture device is OK
     if not capture:
@@ -47,24 +45,24 @@ if __name__ == "__main__":
     # fast forward
     frame_no = 0
     while (frame_no < __start__):
-        frame = cvQueryFrame(capture)
+        frame = cv.QueryFrame(capture)
         frame_no += 1
     
     while 1:
         
         # capture the current frame
-        frame = cvQueryFrame(capture)
-        image_size = cvGetSize(frame)
-        gray = cvCreateImage (cvSize(image_size.width, image_size.height), 8, 1)
-        cvCvtColor( frame, gray, CV_BGR2GRAY )
+        frame = cv.QueryFrame(capture)
+        image_size = cv.GetSize(frame)
+        gray = cv.CreateImage (image_size, 8, 1)
+        cv.CvtColor(frame, gray, cv.CV_BGR2GRAY )
         
         # scale input image for faster processing
-        small_img = cvCreateImage((cvRound(image_size.width/__scale__),
-                                   cvRound(image_size.height/__scale__)), 8, 1 )
-        cvResize(gray, small_img, CV_INTER_LINEAR )
-        cvEqualizeHist( small_img, small_img )
+        small_img = cv.CreateImage((cv.Round(image_size[0]/__scale__),
+                                   cv.Round(image_size[1]/__scale__)), 8, 1 )
+        cv.Resize(gray, small_img, cv.CV_INTER_LINEAR )
+        cv.EqualizeHist( small_img, small_img )
                 
-        s_res = sift(Ipl2NumPy(small_img).flatten('C'))        
+        s_res = sift(np.asarray(small_img).flatten('C'))        
         n_res = array(s_res)
         
         #if frame_no == __start__ or mod(frame_no, 6) == 0:
