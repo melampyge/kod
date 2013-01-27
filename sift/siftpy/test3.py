5c0d5aa1324e99b3c0c353285b8ee551730a257e
burak bayramli <burak@burak-Aspire-S3.(none)>
2013-01-27 23:47:34 +0100

    -

---
+++ sift/siftpy/test3.py
@@ -0,0 +1,16 @@
+import Image
+import sys
+from pylab import *
+from siftpy import *
+
+im=Image.open("../siftpp/data/img3.pgm")
+a = asarray(im).flatten('C')
+print a.shape
+res = sift(a)
+print res
+
+out = open("img3.key", "w")
+for line in res:    
+    out.write(" ".join([str(x) for x in line]))
+    out.write("\n")
+out.close()    
