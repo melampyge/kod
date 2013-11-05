c060541dcef5da913ddfd11f4cd7510a0b877aac
burak <burak@burak-Aspire-V5-122P.(none)>
2013-11-05 16:04:44 +0100

    -

--- jython/mahout.sh
+++
@@ -1,3 +0,0 @@
-CLASSPATH=`find $HOME/.m2/ -name *.jar`
-export CLASSPATH=`echo $CLASSPATH | sed 's/jar\s/jar\:/g'`
-jython test.py
--- jython/weka.sh
+++
@@ -1,3 +0,0 @@
-export CLASSPATH=$CLASSPATH:/home/burak/Downloads/weka-3-6-8/weka.jar
-jython iris1.py
-jython iris2.py
