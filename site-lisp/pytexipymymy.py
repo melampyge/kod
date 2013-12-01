556184e83deb0a72e9e50dc5f2dcca71a5d6b17c
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-01 17:33:14 +0100

    -

--- site-lisp/pytexipymymy.py
+++ site-lisp/pytexipymymy.py
@@ -1,10 +1,14 @@
 from Pymacs import lisp
+import re
 interactions = {}
 
 def run_py_code():
     b = lisp.search_forward("\\end{lstlisting}")
     e = lisp.search_backward("\\begin{lstlisting}")
     content = lisp.buffer_substring(b, e)
+    content = re.sub("\\\\begin{lstlisting}.*?\]","",content)
+    content = re.sub("\\\\end{lstlisting}","",content)
     lisp.message(content)
+    
 
 interactions[run_py_code] = ''
