079b205858c11b6f5e72b5b99ffc434afc428568
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-01 17:51:49 +0100

    -

--- site-lisp/pytexipymymy.py
+++ site-lisp/pytexipymymy.py
@@ -1,3 +1,15 @@
+'''
+INSTALL:
+
+(pymacs-load "/usr/share/emacs23/site-lisp/pytexipymymy")
+(global-set-key [f7] 'pytexipymymy-run-py-code)
+
+When you are in \begin{lstlisting} and \end{lstlisting} blocks, hit
+f7 and all code in that block will be sent to a ipython kernel and
+the result will be displayed. 
+
+'''
+
 from __future__ import print_function
 from StringIO import StringIO
 from IPython.kernel.inprocess.blocking import BlockingInProcessKernelClient
@@ -11,7 +23,9 @@ interactions = {}
 kernels = {}
 
 def get_kernel_pointer(buffer):
+    lisp.message("getting kernel for " + buffer)
     if buffer not in kernels:
+        lisp.message("creating new " + buffer)
         km = InProcessKernelManager()
         km.start_kernel()
         kc = BlockingInProcessKernelClient(kernel=km.kernel)
@@ -22,16 +36,19 @@ def get_kernel_pointer(buffer):
         
 
 def run_py_code():
+    lisp.message("buffer file name " + lisp.buffer_file_name())
+    lisp.message("buffer  name " + lisp.buffer_name())
+    remember_where = lisp.point()
     b = lisp.search_forward("\\end{lstlisting}")
     e = lisp.search_backward("\\begin{lstlisting}")
     content = lisp.buffer_substring(b, e)
     content = re.sub("\\\\begin{lstlisting}.*?\]","",content)
     content = re.sub("\\\\end{lstlisting}","",content)
-
-    kernel = get_kernel_pointer(lisp.buffer_file_name())
+    
+    kernel = get_kernel_pointer(lisp.buffer_name())
     with capture_output() as io:
         kernel.shell.run_cell(content)
     lisp.message(str(io.stdout))
-        
+    lisp.goto_char(remember_where)
 
 interactions[run_py_code] = ''
