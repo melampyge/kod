3323f168a63aff40a2e2bd3ef7ead797e1a78424
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-01 17:38:51 +0100

    -

--- site-lisp/pytexipymymy.py
+++ site-lisp/pytexipymymy.py
@@ -1,6 +1,25 @@
+from __future__ import print_function
+from StringIO import StringIO
+from IPython.kernel.inprocess.blocking import BlockingInProcessKernelClient
+from IPython.kernel.inprocess.manager import InProcessKernelManager
+from IPython.kernel.inprocess.ipkernel import InProcessKernel
+from IPython.utils.io import capture_output
+
 from Pymacs import lisp
-import re
+import re, sys
 interactions = {}
+kernels = {}
+
+def get_kernel_pointer(buffer):
+    if buffer not in kernels:
+        km = InProcessKernelManager()
+        km.start_kernel()
+        kc = BlockingInProcessKernelClient(kernel=km.kernel)
+        kc.start_channels()
+        kernel = InProcessKernel()
+        kernels[buffer] = kernel
+    return kernels[buffer]
+        
 
 def run_py_code():
     b = lisp.search_forward("\\end{lstlisting}")
@@ -8,7 +27,11 @@ def run_py_code():
     content = lisp.buffer_substring(b, e)
     content = re.sub("\\\\begin{lstlisting}.*?\]","",content)
     content = re.sub("\\\\end{lstlisting}","",content)
-    lisp.message(content)
-    
+
+    kernel = get_kernel_pointer(lisp.buffer_file_name())
+    with capture_output() as io:
+        kernel.shell.run_cell(content)
+    lisp.message(str(io.stdout))
+        
 
 interactions[run_py_code] = ''
