54fd8e5dc578dae517774a66e8b72cf3a89a7381
burak <burak@burak-Aspire-V5-122P.(none)>
2013-12-08 11:53:37 +0100

    -

--- scan/scan.py
+++ scan/scan.py
@@ -8,14 +8,12 @@ def run_command(command):
         result.append(l)
     return result
 
-def two_digitize(i):
-    if i < 10: return "0" + str(i)
-    return str(i)
+def twod(i): return "0"+str(i) if i < 10 else str(i)
 
 i = int(sys.argv[1])
 while True:
-    print two_digitize(i)
+    print twod(i)
     run_command("scanimage --mode=Color --resolution 200 -x 215 -y 297 -d plustek:libusb:002:003 --format=tiff > " + two_digitize(i) + ".tiff")
-    run_command("convert -scale %40 " + two_digitize(i) + ".tiff " + two_digitize(i) + ".jpg" )
+    run_command("convert -scale %40 " + twod(i) + ".tiff " + twod(i) + ".jpg" )
     i += 1
     r = raw_input(">")
