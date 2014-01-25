61abfc9f4efdf7e107886c0e4d5958568367c6fe
burak <burak@burak-Aspire-V5-122P.(none)>
2014-01-25 22:22:32 +0100

    -

--- books/Computational_Science_and_Engineering_Strang/simplexcode.m
+++ books/Computational_Science_and_Engineering_Strang/simplexcode.m
@@ -1,13 +1,13 @@
 %8.6  simplexcode.m
 
 function [x,y,cost] = simplex(A,b,c,basis)
-
-B=A(:,basis); x(basis) = B\b;  % xpos at starting corner
-if any (x(basis) < -.000001)
+  disp(basis);
+  B=A(:,basis); x(basis) = B\b;  % xpos at starting corner
+  if any (x(basis) < -.000001)
     error('Starting x has a component < 0')
-end
-cost = c(basis)'*x(basis);     % cost at starting corner
-for iter = 1:100
+  end
+  cost = c(basis)'*x(basis);     % cost at starting corner
+  for iter = 1:100
     y = B'\c(basis);           % this y may not be feasible
     [rmin,in] = min(c - A'*y); % minimum r and its index in
     if rmin >= -.00000001      % optimality is reached, r>=0
@@ -24,4 +24,5 @@ for iter = 1:100
     x(basis) = x(basis) - minratio*v(basis);   % update old x
     x(in) = minratio;      % find new positive component of x
     basis(out) = in;      % replace old index by new in basis
-end
+  end
+
