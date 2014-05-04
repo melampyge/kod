b99af12d20a1fc6b3b815d2b8709fc60ec118e2d
burak <burak@burak-Aspire-V5-122P.(none)>
2014-05-04 16:15:29 +0200

    -

--- scan.py
+++ scan.py
@@ -1,3 +1,10 @@
+'''
+Detect scanner type, start scanning from page 1, dumping results as
+tiff and converting that tiff into jpg in reduced form. the detection
+thing is needed because on my scanner plustek, everytime I plug it
+into USB I get a different code for scanner id which I need to run the
+scanning process
+'''
 import os, re, glob, sys, string;
 
 def run_command(command):
@@ -27,7 +34,7 @@ def two_digitize(i):
     if i < 10: return "0" + str(i)
     return str(i)
 
-i = 0 # by default start with page 0
+i = 1 # by default start with page 1
 if len(sys.argv) > 1: i = int(sys.argv[1])
 
 # detect device
