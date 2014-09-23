075bc2d53bbe688774ace379c9b81f1f2ef35610
burak <burak@burak-Aspire-V5-122P.(none)>
2014-09-23 10:14:37 +0200

    -

--- regfont.sh
+++
@@ -1,2 +0,0 @@
-find . -name '*.tex' -exec perl -pi -e 's/begin{minted}{/begin{minted}[fontsize=\\footnotesize]{/g' {} \;
-find . -name '*.tex' -exec perl -pi -e 's/inputminted{/inputminted[fontsize=\\footnotesize]{/g' {} \;
--- wsync.sh
+++ wsync.sh
@@ -1,5 +1,4 @@
 # Syncs files between coruscant /root/raw folder and local one
 rsync -avzh bbayramli@dev51:/media/data/ps-ml-research /home/burak/Documents/outfit/
 rsync -avzh bbayramli@dev51:/media/data/ps-app-coruscant /home/burak/Documents/outfit/
-rsync -avzh bbayramli@dev51:/media/data/bbayramli/Downloads/prevdata /home/burak/Downloads
 rsync -avzh bbayramli@dev51:/media/data/bbayramli/outfittery-db.txt /home/burak/Dropbox
