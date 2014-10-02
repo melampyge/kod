cdcf92986b085a8fce5f45c766c93f1c8c608e76
burak <burak@burak-Aspire-V5-122P.(none)>
2014-10-02 09:13:41 +0200

    -

--- books/seg-ijcv/segment/run.sh
+++ books/seg-ijcv/segment/run.sh
@@ -1,2 +1,4 @@
-./segment 0.5 500 20 ../../programming_computer_vision_with_python/pcv_book/C-uniform03.ppm /tmp/out.pgm
+#./segment 0.5 500 20 ../../programming_computer_vision_with_python/pcv_book/C-uniform03.ppm /tmp/out.pgm
 #./segment 0.5 450 10000 $HOME/lena.ppm $HOME/lena-out.pgm
+convert /home/burak/Desktop/SE622D02X-K12@3.jpg /home/burak/Desktop/SE622D02X-K12@3.ppm
+./segment 0.5 450 10000 /home/burak/Desktop/SE622D02X-K12@3.ppm /home/burak/Desktop/SE622D02X-K12@3-out.ppm
