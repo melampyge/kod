b07819259abdfd681cf7a5ac58db56026200a175
burak <burak@burak-Aspire-V5-122P.(none)>
2015-01-25 20:00:35 +0100

    -

--- dsync.sh
+++ dsync.sh
@@ -1,8 +1,8 @@
 disk="New Volume"
 #disk="17EA-3758"
 echo "Copying files to /media/$disk/archive"
-rsync  --size-only -P -avzh /home/burak/kod "/media/$disk/archive" --exclude .git
-rsync  --size-only -P -avzh /home/burak/Documents/kitaplar "/media/$disk/archive"
-rsync  -P -avzh /home/burak/Documents/classnotes "/media/$disk/archive" --exclude .git
-rsync  --size-only -P -avzh /home/burak/Dropbox "/media/$disk/archive" 
-rsync  -P -avzh /home/skorsky/mindmeld "/media/$disk/archive"  --exclude .git
+rsync  --size-only  --delete-after -P -avzh /home/burak/kod "/media/$disk/archive" 
+rsync  --size-only  --delete-after -P -avzh /home/burak/Documents/kitaplar "/media/$disk/archive"
+rsync  --size-only  --delete-after -P -avzh /home/burak/Documents/classnotes "/media/$disk/archive" 
+rsync  --size-only  --delete-after -P -avzh /home/burak/Dropbox "/media/$disk/archive" 
+rsync  --size-only  --delete-after -P -avzh /home/skorsky/mindmeld "/media/$disk/archive" 
