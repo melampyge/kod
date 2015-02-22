b53b606afddfb42d73ef29087691b5ddab09e816
burak <burak@burak-Aspire-V5-122P.(none)>
2015-02-22 08:06:23 +0100

    -

--- books/Gelman_BDA2_ARM/arm/5.1.py
+++ books/Gelman_BDA2_ARM/arm/5.1.py
@@ -1,30 +1,18 @@
-import pandas as pd
-import numpy as np
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
-    
-def converter(x):
-    if x == 'NA': return np.nan
-    else: return float(x) 
+import pandas as pd
 
-# read data
-nes = np.loadtxt("nes.dat",  skiprows=1, 
-                    usecols = (1,12,35), 
-                    converters={12:converter, 35:converter}) # year, inc, pvote
+import pandas as pd
+df = pd.read_csv('nes.dat',sep=r'\s+')
+df = df[['presvote','year','income','black']]
+df = df[df['presvote'] < 3] # sadece 2 partinin oylarini al
+# 1,2 oylari 1,0 yap, Cumhuriyetciye verildi mi evet/hayir 
+# haline getir
+df['vote'] = df['presvote'].map(lambda x: x-1) 
+df = df.drop('presvote',axis=1)
+df = df.dropna()
 
-
-nes = nes[nes[:,0] == 1992] # get only the data for 1992
-
-# get only non Nan data
-nes = nes[(nes[:,2] < 3)  | np.isnan(nes[:,2])] # get where presvote < 3 or not nan
-nes[:,2] -= 1 # convert pres vals into 0 or 1 for republicans 
-                        # like gelman now
-
-nes = nes[np.isnan(nes[:,2]) == False] 
-nes = nes[np.isnan(nes[:,1]) == False] # drop nans
-
-df = pd.DataFrame(nes[:,1:], columns = [['income','vote']])
-
-mdlm = smf.logit("vote ~ income", df)
+df2 = df[df['year'] == 1992]
+mdlm = smf.logit("vote ~ income", df2)
 mdlmf = mdlm.fit()
 print(mdlmf.summary())
