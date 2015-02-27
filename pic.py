a15a485b244222d67a197f1fee28c27d4c1ab638
burak <burak@burak-Aspire-V5-122P.(none)>
2015-02-27 14:23:43 +0100

    -

--- pic.py
+++ pic.py
@@ -2,7 +2,7 @@
 
 # Takes a picture. Press 't' to save the screenshot in my-pic-1.jpg,
 # my-pic-2.jpg, etc.. Every press will increment the number.
-import cv
+import cv, time
 
 class Capture:
     
@@ -17,6 +17,7 @@ class Capture:
         print( "Keys:\n"
             "    ESC - quit the program\n"
             "    t - take picture\n"
+            "    o - timed picture\n"
             "To initialize tracking, drag across the object with the mouse\n" )
 
 
@@ -24,7 +25,6 @@ class Capture:
         while True:
             frame = cv.QueryFrame( self.capture )
             cv.ShowImage( "CamShiftDemo", frame )
-
             c = cv.WaitKey(7)
             if c == 27:
                 break
@@ -32,6 +32,20 @@ class Capture:
                 frame = cv.QueryFrame( self.capture )
                 self.save(frame)
                 self.i += 1
+            elif c == ord("o"):
+                # timed pictures, press and it starts taking pictures
+                # every 3 seconds
+                j = 0
+                while True:
+                    j += 1
+                    frame = cv.QueryFrame( self.capture )
+                    cv.ShowImage( "CamShiftDemo", frame )
+                    c = cv.WaitKey(7)
+                    if j % 16 == 0: 
+                        self.save(frame)
+                        self.i += 1
+                        time.sleep(2)
+                
 
 if __name__=="__main__":
     demo = Capture()
