1def9f5e107a50d2c7e34513c63df782e5f941d2
burak <burak@burak-Aspire-V5-122P.(none)>
2015-04-06 14:42:02 +0200

    -

--- books/Logic_of_Political_Survival_Mesquita/test.R
+++ books/Logic_of_Political_Survival_Mesquita/test.R
@@ -1,5 +1,5 @@
 library("lme4")
 df <- read.csv('/tmp/out.csv')
-resp_lmer <- lmer(Klepto ~ W + S + WS + ( 1  | ccode), data = df)
+resp_lmer <- lmer(Klepto ~ WS + log(pop) + ( 1  | year), data = df)
 res <- summary(resp_lmer)
 print (res)
