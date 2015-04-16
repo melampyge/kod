7c0fe6a27b0690a4e1b0f67919d9a7f9aa1353cb
burak <burak@burak-Aspire-V5-122P.(none)>
2015-04-16 12:22:58 +0200

    -

--- hipy/rndplay.py
+++ hipy/rndplay.py
@@ -17,7 +17,7 @@ while True:
     fout.write(str(list[idx]) + "\n")
     fout.flush()
     print '\n'
-    if ".avi" in list[idx] or ".mp4" in list[idx]:
+    if ".avi" in list[idx] or ".mp4" in list[idx] or ".mkv" in list[idx] :
         os.system("mplayer  -vf scale -zoom -xy 1100 '%s'" % list[idx] )
     else:
         os.system("mplayer '%s'" % list[idx] )
