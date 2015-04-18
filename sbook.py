acf0af3c6effa438d3faf37d8264a964e35090aa
burak <burak@burak-Aspire-V5-122P.(none)>
2015-04-18 23:44:33 +0200

    -

--- sbook.py
+++ sbook.py
@@ -16,11 +16,11 @@ run_command("convert -unsharp 1.5x1.2+1.0+0.10 -crop 300x300+10+0 01.tiff /tmp/g
 run_command("convert -monochrome /tmp/gray.tiff /tmp/mono.tiff")
 run_command("cjb2 -clean /tmp/mono.tiff /home/burak/book.djvu")
 
-for a in range(30):
+for a in range(5):
     i = a + 1
-    file1 = str(two_digitize(i)) + ".tiff"
-    print file1
-    run_command("convert -unsharp 1.5x1.2+1.0+0.10  " + str(file1) + " /tmp/gray.tiff")
+    file = str(two_digitize(i)) + ".tiff"
+    print file
+    run_command("convert -unsharp 1.5x1.2+1.0+0.10 %s /tmp/gray.tiff" % file)
     run_command("convert -monochrome /tmp/gray.tiff /tmp/mono.tiff")
     run_command("cjb2 -clean /tmp/mono.tiff /tmp/page.djvu")
     run_command("djvm -i /home/burak/book.djvu /tmp/page.djvu")
