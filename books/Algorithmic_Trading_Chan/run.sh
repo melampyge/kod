f00e8510817c7f5ba8e8fb3e31dac91382071144
Burak Bayramli <you@example.com>
2015-06-15 23:26:41 +0200

    -

--- books/Algorithmic_Trading_Chan/run.sh
+++ books/Algorithmic_Trading_Chan/run.sh
@@ -1,1 +1,6 @@
-/Applications/MATLAB_R2014b.app/bin/matlab  -nodisplay -nosplash -nodesktop -r  $1
+if [ `uname` = "Linux" ]
+then
+    $HOME/matlab/bin/matlab  -nodisplay -nosplash -nodesktop -r  $1
+else
+    /Applications/MATLAB_R2014b.app/bin/matlab  -nodisplay -nosplash -nodesktop -r  $1
+fi
