8c49d078b5487a649312a734778479914b714265
Burak Bayramli <bb@bb.com>
2015-06-16 20:56:05 +0200

    changes

--- books/Algorithmic_Trading_Chan/run.sh
+++ books/Algorithmic_Trading_Chan/run.sh
@@ -1,3 +1,2 @@
-JPLV7=/Users/burakbayramli/Downloads/jplv7
-export MATLABPATH=$JPLV7/util:$JPLV7/regress:$JPLV7/distrib
+export MATLABPATH=./jplv7/util:./jplv7/regress:./jplv7/distrib
 /Applications/MATLAB_R2014b.app/bin/matlab  -nodisplay -nosplash -nodesktop -r  $1
