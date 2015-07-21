115955a095b8402641731c10468e3fe20f7d9537
Burak Bayramli <you@example.com>
2015-07-21 16:34:36 +0200

    -

--- site-lisp/ipython-tex.py
+++ site-lisp/ipython-tex.py
@@ -78,7 +78,7 @@ def run_py_code():
     plt_count_before = len(re.findall('plt\.savefig\(',bc))
     base = os.path.splitext(lisp.buffer_name())[0]
     f = '%s_%s.png' % (base, two_digit(plt_count_before+1))
-    rpl = "plt.hold(False); plt.savefig('%s'); plt.hold(False)" % f
+    rpl = "plt.hold(False)\nplt.savefig('%s')\nplt.hold(False)" % f
     show_replaced = True if "plt.show()" in content else False
     content=content.replace("plt.show()",rpl)
     include_graphics_command = "\\includegraphics[height=6cm]{%s}" % f
