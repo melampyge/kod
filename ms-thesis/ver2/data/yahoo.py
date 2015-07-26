9b2d6abaab8cfc952ab6108f6604675b48cdf1ec
Burak Bayramli <you@example.com>
2015-07-26 10:14:25 +0200

    -

--- ms-thesis/ver2/data/yahoo.py
+++ ms-thesis/ver2/data/yahoo.py
@@ -2,13 +2,13 @@ import datetime
 import pandas as pd
 import pandas.io.data
 
-# Yahoo Finance
 df = pd.io.data.get_data_yahoo('XU100.IS', 
                                  start=datetime.datetime(1999, 4, 29), 
-                                 end=datetime.datetime(2015, 6, 1))
+                                 end=datetime.datetime(2015, 7, 26))
 df.to_csv('ise.csv')
 
-# FRED
-#jpy = pd.io.data.DataReader('DEXUSEU', 'fred')
-#jpy.to_csv('/tmp/DEXJPUS.csv')
+df = pd.io.data.get_data_yahoo('^DJI', 
+                                 start=datetime.datetime(1990, 8, 28), 
+                                 end=datetime.datetime(2015, 7, 26))
+df.to_csv('dji.csv')
 
