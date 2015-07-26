7c5c459b8704f2f40e86005476952f9becaa1edf
Burak Bayramli <you@example.com>
2015-07-26 10:30:51 +0200

    -

--- ms-thesis/ver2/build.py
+++ ms-thesis/ver2/build.py
@@ -1,7 +1,7 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex -shell-escape ms-the*.tex")
+    os.system("pdflatex -shell-escape ms-thesis.tex")
     os.system("evince ms-the*.pdf")
     exit()
     
