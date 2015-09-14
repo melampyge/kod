78da766c030f434f5526360e938560229a20a581
Burak Bayramli <you@example.com>
2015-09-14 16:07:00 +0200

    -

--- books/Algorithmic_Trading_Chan/tmp.m
+++ books/Algorithmic_Trading_Chan/tmp.m
@@ -1,44 +1,39 @@
 clear;
 
-load('inputDataOHLCDaily_20120511', 'syms', 'tday', 'cl');
+entryZscore=0.1;
 
-% load('//dellquad/Futures_data/inputDataOHLCDaily_20120815', 'syms', 'tday', 'cl');
-idx=strmatch('TU', syms, 'exact');
-tday=tday(:, idx);
-cl=cl(:, idx);
+data=load('inputDataOHLCDaily_20120517', 'syms', 'tday', 'op', 'hi', 'lo', 'cl');
+idx=find(strcmp('FSTX', data.syms));
 
-lookback=250;
-holddays=25;
+op=data.op(:, idx);
+hi=data.hi(:, idx);
+lo=data.lo(:, idx);
+cl=data.cl(:, idx);
+%date=data.tday(:, idx);
+%A = [date op hi lo cl];
+%save('/tmp/FSTX','A')
+%exit;
 
-longs=cl > backshift(lookback, cl)  ;
-shorts=cl < backshift(lookback, cl) ;
+stdretC2C90d=backshift(1, smartMovingStd(calculateReturns(cl, 1), 90));
 
-pos=zeros(length(cl), 1);
+longs= op >= backshift(1, hi).*(1+entryZscore*stdretC2C90d);
+shorts=op <= backshift(1, lo).*(1-entryZscore*stdretC2C90d);
 
-for h=0:holddays-1
-    long_lag=backshift(h, longs);
-    long_lag(isnan(long_lag))=false;
-    long_lag=logical(long_lag);
-    
-    short_lag=backshift(h, shorts);
-    short_lag(isnan(short_lag))=false;
-    short_lag=logical(short_lag);
-    
-    pos(long_lag)=pos(long_lag)+1;
-    pos(short_lag)=pos(short_lag)-1;
-end
+positions=zeros(size(cl));
 
-ret=(backshift(1, pos).*(cl-backshift(1, cl))./backshift(1, cl))/holddays;
+positions(longs)=1;
+positions(shorts)=-1;
 
+ret=positions.*(op-cl)./op;
 ret(isnan(ret))=0;
-% idx=find(tday==20090102);
-idx=1;
 
-cumret=cumprod(1+ret(idx:end))-1;
+fprintf(1, '%s APR=%10.4f Sharpe=%4.2f\n', data.syms{idx}, prod(1+ret).^(252/length(ret))-1, mean(ret)*sqrt(252)/std(ret));
+% APR=    0.1327 Sharpe=1.44
+cumret=cumprod(1+ret)-1; % compounded ROE
 
-fprintf(1, 'Avg Ann Ret=%7.4f Ann Volatility=%7.4f Sharpe ratio=%4.2f \n',252*smartmean(ret(idx:end)), sqrt(252)*smartstd(ret(idx:end)), sqrt(252)*smartmean(ret(idx:end))/smartstd(ret(idx:end)));
-fprintf(1, 'APR=%10.4f\n', prod(1+ret(idx:end)).^(252/length(ret(idx:end)))-1);
+plot(cumret);
+
+
 [maxDD maxDDD]=calculateMaxDD(cumret);
 fprintf(1, 'Max DD =%f Max DDD in days=%i\n\n', maxDD, round(maxDDD));
-fprintf(1, 'Kelly f=%f\n', mean(ret(idx:end))/std(ret(idx:end))^2);
 
