c0f40cf7c10479b6815a222d99ed065140288fda
Burak Bayramli <you@example.com>
2015-09-17 15:50:58 +0200

    -

--- books/Algorithmic_Trading_Chan/tmp.m
+++ books/Algorithmic_Trading_Chan/tmp.m
@@ -1,20 +1,12 @@
 clear;
 
-% 1 minute data on GLD-USO
-load('inputData_ETF', 'tday', 'syms', 'cl');
-idxG=find(strcmp('GLD', syms));
-idxU=find(strcmp('USO', syms));
+load('inputDataOHLCDaily_20120511', 'syms', 'tday', 'cl');
+% load('//dellquad/Futures_data/inputDataOHLCDaily_20120815', 'syms', 'tday', 'cl');
+idx=strmatch('TU', syms, 'exact');
 
-x=cl(:, idxG);
-y=cl(:, idxU);
+tday=tday(:, idx);
+cl=cl(:, idx);
 
-lookback=20; % Lookback set arbitrarily short
-hedgeRatio=NaN(size(x, 1), 1);
-for t=lookback:size(hedgeRatio, 1)
-    tmp1 = [x(t-lookback+1:t) ones(lookback, 1)]
-    yy = y(t-lookback+1:t)
-    regression_result=ols(yy, tmp1);
-    hedgeRatio(t)=regression_result.beta(1);
-    disp(hedgeRatio(t));
-end
-
+A = [tday cl];
+save('/tmp/out','A');
+exit;
