2956198effbd801f7f524058430d47822b61a519
Burak Bayramli <you@example.com>
2015-09-17 03:05:44 +0200

    -

--- books/Algorithmic_Trading_Chan/tmp.m
+++ books/Algorithmic_Trading_Chan/tmp.m
@@ -1,39 +1,22 @@
 clear;
 
-entryZscore=0.1;
+% 1 minute data on USDCAD
+load('/home/burak/Dropbox/Public/data/inputData_USDCAD_20120426', 'tday', 'hhmm', 'cl');
 
-data=load('inputDataOHLCDaily_20120517', 'syms', 'tday', 'op', 'hi', 'lo', 'cl');
-idx=find(strcmp('FSTX', data.syms));
+% Select the daily close at 16:59 ET.
+y=cl(hhmm==1659);
 
-op=data.op(:, idx);
-hi=data.hi(:, idx);
-lo=data.lo(:, idx);
-cl=data.cl(:, idx);
-%date=data.tday(:, idx);
-%A = [date op hi lo cl];
-%save('/tmp/FSTX','A')
-%exit;
+save('/tmp/y','y')
 
-stdretC2C90d=backshift(1, smartMovingStd(calculateReturns(cl, 1), 90));
+% Variance ratio test from Matlab Econometrics Toolbox
+[h,pValue]=vratiotest(log(y))
 
-longs= op >= backshift(1, hi).*(1+entryZscore*stdretC2C90d);
-shorts=op <= backshift(1, lo).*(1-entryZscore*stdretC2C90d);
+% Find value of lambda and thus the halflife of mean reversion by linear regression fit
+ylag=lag(y, 1);  % lag is a function in the jplv7 (spatial-econometrics.com) package.
+deltaY=y-ylag;
+deltaY(1)=[]; % Regression functions cannot handle the NaN in the first bar of the time series.
+ylag(1)=[];
+regress_results=ols(deltaY, [ylag ones(size(ylag))]); % ols is a function in the jplv7 (spatial-econometrics.com) package.
+halflife=-log(2)/regress_results.beta(1);
 
-positions=zeros(size(cl));
-
-positions(longs)=1;
-positions(shorts)=-1;
-
-ret=positions.*(op-cl)./op;
-ret(isnan(ret))=0;
-
-fprintf(1, '%s APR=%10.4f Sharpe=%4.2f\n', data.syms{idx}, prod(1+ret).^(252/length(ret))-1, mean(ret)*sqrt(252)/std(ret));
-% APR=    0.1327 Sharpe=1.44
-cumret=cumprod(1+ret)-1; % compounded ROE
-
-plot(cumret);
-
-
-[maxDD maxDDD]=calculateMaxDD(cumret);
-fprintf(1, 'Max DD =%f Max DDD in days=%i\n\n', maxDD, round(maxDDD));
-
+fprintf(1, 'halflife=%f days\n', halflife);
