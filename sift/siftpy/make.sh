f7c3dea0cd4c3d27f22da105f78d6fb09f8dd8ef
Burak Bayramli <you@example.com>
2015-11-30 08:56:51 +0100

    -

--- sift/siftpy/make.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-SIFT_DIR=/home/burak/kod/sift/siftpp
-g++ -fPIC -Wall -g -O3 -DNDEBUG -Wno-variadic-macros -DVL_LOWE_STRICT -DVL_USEFASTMATH -shared -o siftpy.so -I$SIFT_DIR -I/usr/include/python2.7 -lpython2.7 siftpy.c 
