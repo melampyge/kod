3f351cb9ed6dbd3f34de117c2aece697d4e89c53
Burak Bayramli <you@example.com>
2015-12-12 21:53:38 +0100

    -

--- news.sh
+++ news.sh
@@ -1,3 +1,3 @@
 # prepares an xterm for newsbeuter
-xterm -fn *-fixed-*-*-*-20-* -maximize -e "newsbeuter -r"
-
+#xterm -fn *-fixed-*-*-*-20-* -maximize -e "newsbeuter -r"
+xterm -fn -misc-fixed-medium-r-normal--20-200-75-75-c-100-iso8859-15 -maximize -e "newsbeuter -r"
