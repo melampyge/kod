f22a5e5b747294aa34ac7438470deda181155fa2
Burak Bayramli <you@example.com>
2015-12-29 20:37:43 +0100

    -

--- site-lisp/deascify.py
+++ site-lisp/deascify.py
@@ -3,11 +3,22 @@ import re, sys, time, os
 interactions = {}
 from turkish.deasciifier import Deasciifier
 
-def convert():
+def get_block_content(start_tag, end_tag):
     remember_where = lisp.point()
-    s = u"Bogurtuler opucukler."
-    dea = Deasciifier(s)
-    result = dea.convert_to_turkish()
-    lisp.message(result)
+    block_begin = lisp.search_backward(start_tag)
+    block_end = lisp.search_forward(end_tag)
+    block_end = lisp.search_forward(end_tag)
+    content = lisp.buffer_substring(block_begin, block_end)
+    lisp.message(content)
+    lisp.goto_char(remember_where)
+    return block_begin, block_end, content
+
+def convert():
+    block_begin, block_end, content = get_block_content("\n\n","\n\n")
+    #s = u"Bogurtuler opucukler."
+    #dea = Deasciifier(s)
+    #result = dea.convert_to_turkish()
+    lisp.message(content)
+    #lisp.insert(content)
             
 interactions[convert] = ''
